a00c83e9df4e5b4fe3b0329435561c7381d0a1c1
Romain S <romain.simon@artebeaute.com>
2022-02-17 15:12:18 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      fixed
+      app
+      elevation="0"
+      dark
+    >
+      <v-toolbar-title>
+        Image Reducer
+      </v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container class="py-8">
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <v-form @submit.prevent="handleForm">
+          <v-row>
+            <v-col cols="12" sm="10">
+              <v-file-input v-model="file" filled dense hide-details />
+            </v-col>
+            <v-col cols="12" sm="2">
+              <v-btn type="submit" color="primary" elevation="0" block>
+                Valider
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-col>
+      <v-col v-if="fileBase64" cols="12">
+        <v-row>
+          <v-col cols="12" sm="6">
+            <img :src="fileBase64" width="100%">
+          </v-col>
+          <v-col cols="12" sm="6">
+            <p>
+              <strong>Original File Size :</strong>
+              {{ file.size }}
+            </p>
+            <v-btn type="button" color="primary" elevation="0" @click="handleCompress">
+              Compresser
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col v-if="compressedFileBase64" cols="12">
+        <v-row>
+          <v-col cols="12" sm="6">
+            <img :src="compressedFileBase64" width="100%">
+          </v-col>
+          <v-col cols="12" sm="6">
+            <p>
+              <strong>Original File Size :</strong>
+              {{ compressedFile.size }}
+            </p>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      file: null,
+      fileBase64: null,
+
+      compressedFile: null,
+      compressedFileBase64: null
+    }
+  },
+  methods: {
+    async handleForm () {
+      this.fileBase64 = await this.toBase64(this.file)
+    },
+    async handleCompress () {
+      const Compress = require('client-compress')
+
+      const options = {
+        targetSize: 0.2,
+        quality: 0.75,
+        maxWidth: 1920,
+        maxHeight: 1080
+      }
+
+      const compress = new Compress(options)
+
+      const compressedData = await compress.compress([this.file], {
+        size: 0.2, // the max size in MB, defaults to 2MB
+        quality: 1, // the quality of the image, max is 1,
+        maxWidth: 1920, // the max width of the output image, defaults to 1920px
+        maxHeight: 1080, // the max height of the output image, defaults to 1920px
+        resize: true, // defaults to true, set false if you do not want to resize the image width and height
+        rotate: false // See the rotation section below
+      })
+
+      this.compressedFile = compressedData[0].photo.data
+      this.compressedFileBase64 = await this.toBase64(this.compressedFile)
+    },
+    toBase64 (file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = error => reject(error)
+      })
+    }
+  }
+}
+</script>
